93749c5ab7356957e99119846a20af37615dfbb1
ZakharKolyada <zakhar.kolyada@apriorit.com>
2024-11-29 09:44:21 +0200

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "public_ip_lb" {
-  value = azurerm_public_ip.example_lb_public_ip.ip_address
-}
-
-output "vm_private_ips" {
-  value = azurerm_network_interface.example_nic[*].private_ip_address
-}
